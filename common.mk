89e297fd7dc7ccc7ceaea5c8a5b3177d0522c56d
Zach Bacon <11doctorwhocanada@gmail.com>
2015-12-02 20:13:21 -0500

    will be editing flags later.

--- common.mk
+++ common.mk
@@ -1,6 +1,1 @@
-AM_CFLAGS = -fstack-protector -Wall -pedantic \
-        -Wstrict-prototypes -Wundef -fno-common \
-        -Werror-implicit-function-declaration \
-        -Wformat -Wformat-security -Werror=format-security \
-        -Wno-conversion -Wunused-variable -Wunreachable-code \
-        -std=c99 -Werror
+AM_CFLAGS = -flto -std=c99
