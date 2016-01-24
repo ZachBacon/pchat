01085f5bccd1223d1dae06ed4af8e7cb39e2346d
Zach Bacon <11doctorwhocanada@gmail.com>
2016-01-23 19:01:29 -0500

    small make changes

--- src/common/pchat-platform-support.c
+++ src/common/pchat-platform-support.c
@@ -8,45 +8,26 @@
 
 #include "pchat-platform-support.h"
 
-gchar *
+char *
 pchat_platform_support_get_lib_dir (void)
 {
 	return g_strdup (XCHATLIBDIR);
 }
 
-gchar *
+char *
 pchat_platform_support_get_locale_dir (void)
 {
 	return g_strdup (ISO_CODES_LOCALEDIR);
 }
 
-gchar *
+char *
 pchat_platform_support_get_data_dir (void)
 {
-	return g_strdup (PCHAT_DATADIR);
+	return g_strdup (ISO_CODES_LOCALEDIR);
 }
 
-gchar *
-pchat_platform_support_get_user_home_dir (const gchar *name)
+char *
+pchat_platform_support_get_user_home_dir (const char *name)
 {
-	struct passwd *pwd;
-
-	if (name == NULL)
-	{
-		name = g_get_user_name ();
-	}
-
-	if (name == NULL)
-	{
-		return NULL;
-	}
-
-	pwd = getpwnam (name);
-
-	if (pwd == NULL)
-	{
-		return NULL;
-	}
-
-	return g_strdup (pwd->pw_dir);
+	return NULL;
 }
